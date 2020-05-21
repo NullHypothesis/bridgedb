NullHypothesis/bridgedb

commit c1a48d1b568b00fab19a308e6497881f31d17680

Fix context manager creation
BridgeDB subclassed a GeneratorContextManager class which became private in
python 3.x. Its constructor signature changed, so attempting to simply use
the private version failed with...

  Traceback (most recent call last):
    File "/home/atagar/Desktop/tor/bridgedb/bridgedb/test/test_Storage.py", line 76, in test_insertBridgeAndGetRing_already_seen_bridge
      with Storage.getDB() as db:
    File "/home/atagar/Desktop/tor/bridgedb/bridgedb/Storage.py", line 400, in helper
      return DBGeneratorContextManager(func(*args, **kwds))
  builtins.TypeError: __init__() missing 2 required positional arguments: 'args' and 'kwds'

Storage.py is pretty confusing, but they seem to simply want a context manager
so simply implementing one.

This changes the test results as follows...

  before: FAILED (skips=106, failures=16, errors=272, successes=410)
  after:  FAILED (skips=106, failures=17, errors=266, successes=415)

bridgedb/Storage.py
@@ -8,7 +8,6 @@ import binascii
 import sqlite3
 import time
 import hashlib
-from contextlib import _GeneratorContextManager
 from functools import wraps
 from ipaddr import IPAddress
 import sys
@@ -345,11 +344,18 @@ def openDatabase(sqlite_file):
     return conn
 
 
-class DBGeneratorContextManager(_GeneratorContextManager):
+class DBGeneratorContextManager(object):
     """Helper for @contextmanager decorator.
 
     Overload __exit__() so we can call the generator many times
     """
+
+    def __init__(self, gen):
+      self.gen = gen
+
+    def __enter__(self):
+      return next(self.gen)
+
     def __exit__(self, type, value, traceback):
         """Handle exiting a with statement block
 

commit 12f51f2ce2308d456192d8fd99341820aa29021e

Use contextlib's context manager.
So far, the email autoresponder fails after a while, raising the
following exception:

  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/twisted/internet/posixbase.py", line 614, in _doReadOrWrite
    why = selectable.doRead()
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/twisted/internet/tcp.py", line 243, in doRead
    return self._dataReceived(data)
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/twisted/internet/tcp.py", line 249, in _dataReceived
    rval = self.protocol.dataReceived(data)
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/twisted/protocols/basic.py", line 454, in dataReceived
    self.lineReceived(line)
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/twisted/mail/smtp.py", line 445, in lineReceived
    return getattr(self, 'state_' + self.mode)(line)
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/twisted/mail/smtp.py", line 705, in dataLineReceived
    m.eomReceived() for m in self.__messages
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/twisted/mail/smtp.py", line 705, in <listcomp>
    m.eomReceived() for m in self.__messages
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/bridgedb-0.10.0+11.g4cdd6a61.dirty-py3.7.egg/bridgedb/distributors/email/server.py", line 230, in eomReceived
    self.responder.reply()
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/bridgedb-0.10.0+11.g4cdd6a61.dirty-py3.7.egg/bridgedb/distributors/email/autoresponder.py", line 574, in reply
    response = self.getMailData()
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/bridgedb-0.10.0+11.g4cdd6a61.dirty-py3.7.egg/bridgedb/distributors/email/autoresponder.py", line 392, in getMailData
    client, lang)
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/bridgedb-0.10.0+11.g4cdd6a61.dirty-py3.7.egg/bridgedb/distributors/email/autoresponder.py", line 101, in createResponseBody
    bridges = context.distributor.getBridges(bridgeRequest, interval)
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/bridgedb-0.10.0+11.g4cdd6a61.dirty-py3.7.egg/bridgedb/distributors/email/distributor.py", line 145, in getBridges
    with bridgedb.Storage.getDB() as db:
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/bridgedb-0.10.0+11.g4cdd6a61.dirty-py3.7.egg/bridgedb/Storage.py", line 352, in __enter__
    return next(self.gen)
  File "/home/bridgedb/virtualenvs/bridgedb/lib/python3.7/site-packages/bridgedb-0.10.0+11.g4cdd6a61.dirty-py3.7.egg/bridgedb/Storage.py", line 472, in getDB
    assert _REFCOUNT == 0
builtins.AssertionError:

It's not clear what caused this regression but it may have been
introduced in commit c1a48d1, as part of our conversion to Python 3.

bridgedb/Storage.py
@@ -10,6 +10,7 @@ import time
 import hashlib
 from functools import wraps
 from ipaddr import IPAddress
+from contextlib import contextmanager
 import sys
 
 from bridgedb.Stability import BridgeHistory
@@ -338,68 +339,6 @@ def openDatabase(sqlite_file):
         cur.close()
     return conn
 
-
-class DBGeneratorContextManager(object):
-    """Helper for @contextmanager decorator.
-
-    Overload __exit__() so we can call the generator many times
-    """
-
-    def __init__(self, gen):
-      self.gen = gen
-
-    def __enter__(self):
-      return next(self.gen)
-
-    def __exit__(self, type, value, traceback):
-        """Handle exiting a with statement block
-
-        Progress generator or throw exception
-
-        Significantly based on contextlib.py
-
-        :throws: `RuntimeError` if the generator doesn't stop after
-            exception is thrown
-        """
-        if type is None:
-            try:
-                next(self.gen)
-            except StopIteration:
-                return
-            return
-        else:
-            if value is None:
-                # Need to force instantiation so we can reliably
-                # tell if we get the same exception back
-                value = type()
-            try:
-                self.gen.throw(type, value, traceback)
-                raise RuntimeError("generator didn't stop after throw()")
-            except StopIteration as exc:
-                # Suppress the exception *unless* it's the same exception that
-                # was passed to throw().  This prevents a StopIteration
-                # raised inside the "with" statement from being suppressed
-                return exc is not value
-            except:
-                # only re-raise if it's *not* the exception that was
-                # passed to throw(), because __exit__() must not raise
-                # an exception unless __exit__() itself failed.  But throw()
-                # has to raise the exception to signal propagation, so this
-                # fixes the impedance mismatch between the throw() protocol
-                # and the __exit__() protocol.
-                #
-                if sys.exc_info()[1] is not value:
-                    raise
-
-def contextmanager(func):
-    """Decorator to for :func:`Storage.getDB()`
-
-    Define getDB() for use by with statement content manager
-    """
-    @wraps(func)
-    def helper(*args, **kwds):
-        return DBGeneratorContextManager(func(*args, **kwds))
-    return helper
 
 _DB_FNAME = None
 _LOCK = None